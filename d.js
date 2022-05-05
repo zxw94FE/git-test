ac1a35f0c8cc256208135a952fa00805616b7173
zhaixiaowei <zhaixiaowei@xdf.cn>
2022-05-05 15:39:38 +0800

    Revert "feat: d的提交"
    This reverts commit ba2f4a57211bd75127566eb9314391c77e34cf6c.

--- d.js
+++
@@ -1,7 +0,0 @@
-/*
- * @Author       : zhaixiaowei@xdf.cn
- * @Date         : 2022-05-05 15:38:12
- * @LastEditors  : zhaixiaowei@xdf.cn
- * @LastEditTime : 2022-05-05 15:38:12
- * @Description  : 描述信息
- */
